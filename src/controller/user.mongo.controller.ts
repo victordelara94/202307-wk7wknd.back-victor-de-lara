a7aac2ac7c804ee07c4eb3da41dd19d8a5fbde90
Víctor de Lara Martínez <vdelara.afd@gmail.com>
2023-09-02 15:44:54 +0200

    add login and authorizate methods

--- src/controller/user.mongo.controller.ts
+++ src/controller/user.mongo.controller.ts
@@ -4,6 +4,8 @@ import { NextFunction, Request, Response } from 'express';
 import { User } from '../entities/user';
 import { Repository } from '../repository/repository';
 import { Auth } from '../services/auth';
+import { HttpError } from '../types/http.error.type';
+import { TokenPayload } from '../types/token.type';
 const debug = createDebug('SN:Controller:UserController');
 export class UserMongoController {
   constructor(private repo: Repository<User>) {
@@ -19,13 +21,26 @@ export class UserMongoController {
     } catch (error) {
       next(error);
     }
-
-    // Login(){
-
-    // }
-    // getAll();
-    // getById();
-    // update();
-    // delete();
   }
+
+  async Login(req: Request, res: Response, next: NextFunction) {
+    const { userName, password } = req.body;
+    const error = new HttpError(401, 'UnAuthorized', 'Login unauthorized');
+    try {
+      const data = await this.repo.search({ key: 'userName', value: userName });
+      if (!data.length) throw error;
+      const payload: TokenPayload = {
+        userName: data[0].userName,
+        id: data[0].id,
+      };
+      const token = await Auth.signJWT(payload);
+      res.json({ user: data[0], token });
+    } catch (error) {
+      next(error);
+    }
+  }
+  // GetAll();
+  // getById();
+  // update();
+  // delete();
 }
